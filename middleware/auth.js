091f9cfd9a9aa21c2af976f7a9314e109a43e073
yalaouihicham <yalaouihicham13@gmail.com>
2022-02-13 20:04:08 +0100

    modification de racourci

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,14 +1,12 @@
 //-------------- Authentifiaction --------------
 
 const jwt = require('jsonwebtoken');
-require('dotenv').config();
-
 
 module.exports = (req, res, next) => {
     try{
         const token = req.headers.authorization.split(' ')[1];
         const decodedToken = jwt.verify(token, process.env.RANDOM_SECRET_KEY);
-        const userId = decodedToken.userId;
+        const userId = decodedToken.userId;// on vient recupérer notre userId
         req.auth = { userId };
         // -------- vérifier l'identité de l'utilisateur
         if(req.body.userId && req.body.userId !== userId) {
